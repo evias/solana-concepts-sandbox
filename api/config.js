3736ad64030d07f639a14b6c3ea1870d885d20a2
evias <141805+evias@users.noreply.github.com>
2026-06-29 13:48:40 +0200

    conf: add SCS_BUILD env configuration

--- api/config.js
+++ api/config.js
@@ -7,18 +7,19 @@ require('dotenv').config();
 
 module.exports = {
   server: {
+    buildType: process.env.SCS_BUILD || 'development',
     bindHost: process.env.SCS_BIND_HOST || 'localhost',
     bindPort: parseInt(process.env.SCS_BIND_PORT || '3000', 10)
   },
-  
+
   uploads: {
     path: process.env.SCS_UPLOADS_PATH || 'uploads/'
   },
-  
+
   payer: {
     keypairFile: process.env.SCS_KEYPAIR_FILE || '.payer-keypair.json'
   },
-  
+
   logging: {
     logFile: process.env.SCS_LOGS_FILE || 'sandbox.log'
   }
